eb6b4d1b7601df5603faaff4a7ca31bb74b1ccbc
Sundararajan Thangaraj <geeksundar1993@gmail.com>
2023-12-07 18:09:26 +0530

    Adding two pages

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/products', "methods=['GET']")
+def products():
+    return render_template("products.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
